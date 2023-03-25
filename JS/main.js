b1340d65a5858d4837d4cd33f5f66214d3214784
StevenEduardoReyesLeon <stevenrl325@gmail.com>
2023-03-25 07:29:56 -0500

    PUNTO CUATRO

--- JS/main.js
+++ JS/main.js
@@ -6,8 +6,13 @@ export default {
             this.calcular(...taller.data());
         })
     },
-    calcular(r,h) {
-
-        alert(`El Area Lateral del Cilindro de Radio ${r} y Altura ${h} es ${2 * Math.PI * r * h} y su Volumen es ${Math.PI * Math.pow(r,2) * h}`);
+    calcular(n1,n2) {
+        if (n1==n2) {
+            alert(`El numero ${n1} es igual al número ${n2}`);
+        } else if (n1>n2) {
+            alert(`Los números ordenados son : ${n2}, ${n1}`)
+        } else {
+            alert(`Los números ordenados son: ${n1}, ${n2}`);
+        }
     }
 }
--- JS/taller.js
+++ JS/taller.js
@@ -1,8 +1,8 @@
 
 export default {
 
-    num1: document.querySelector("#radio"),
-    num2: document.querySelector("#altura"),
+    num1: document.querySelector("#numero1"),
+    num2: document.querySelector("#numero2"),
     data() {
         return [
             Number(this.num1.value),
